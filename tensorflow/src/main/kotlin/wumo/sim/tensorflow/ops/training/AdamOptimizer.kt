10d26fc6527698bef0d4759586f380d833acfab1
wumo <wumo@outlook.com>
2018-09-04 17:49:50 +0800

    adamOptimizer

--- tensorflow/src/main/kotlin/wumo/sim/tensorflow/ops/training/AdamOptimizer.kt
+++ tensorflow/src/main/kotlin/wumo/sim/tensorflow/ops/training/AdamOptimizer.kt
@@ -1,4 +1,153 @@
 package wumo.sim.tensorflow.ops.training
+
+import wumo.sim.tensorflow.ops.*
+import wumo.sim.tensorflow.ops.variables.Variable
+import wumo.sim.tensorflow.tensor.Tensor
+import wumo.sim.tensorflow.tf
+
+/** Optimizer that implements the Adam optimization algorithm.
+ *
+ * Initialization:
+ * {{{
+ *   m_0 = 0  // Initialize the 1st moment vector
+ *   v_0 = 0  // Initialize the 2nd moment vector
+ *   t = 0    // Initialize the time step
+ * }}}
+ *
+ * The Adam update for step `t` is as follows:
+ * {{{
+ *   learningRate_t = initialLearningRate * sqrt(beta1 - beta2^t) / (1 - beta1^t)
+ *   m_t = beta1 * m_{t-1} + (1 - beta1) * gradient
+ *   v_t = beta2 * v_{t-1} + (1 - beta2) * gradient * gradient
+ *   variable -= learningRate_t * m_t / (sqrt(v_t) + epsilon)
+ * }}}
+ *
+ * The default value of `1e-8` for epsilon might not be a good default in general. For example, when training an
+ * Inception network on ImageNet a current good choice is `1.0` or `0.1`. Note that since the Adam optimizer uses the
+ * formulation just before Section 2.1 of the [Kingma and Ba paper](https://arxiv.org/abs/1412.6980) rather than the
+ * formulation in Algorithm 1, the "epsilon" referred to here is "epsilon hat" in the paper.
+ *
+ * The sparse implementation of this algorithm (used when the gradient is an indexed slices object, typically because
+ * of `tf.gather` or an embedding lookup in the forward pass) does apply momentum to variable slices even if they were
+ * not used in the forward pass (meaning they have a gradient equal to zero). Momentum decay (`beta1`) is also applied
+ * to the entire momentum accumulator. This means that the sparse behavior is equivalent to the dense behavior (in
+ * contrast to some momentum implementations which ignore momentum unless a variable slice was actually used).
+ *
+ * For more information on this algorithm, please refer to this [paper](https://arxiv.org/abs/1412.6980)
+ * ([PDF](https://arxiv.org/pdf/1412.6980.pdf)).
+ *
+ * @param  learningRate           Learning rate. Must be `> 0`. If used with `decay`, then this argument
+ *                                specifies the initial value of the learning rate.
+ * @param  decay                  Learning rate decay method to use for each update.
+ * @param  beta1                  Exponential decay rate for the first moment estimates.
+ * @param  beta2                  Exponential decay rate for the second moment estimates.
+ * @param  useNesterov            If `true`, Nesterov momentum is used for the updates.
+ * @param  epsilon                Small constant used for numerical stability. This epsilon corresponds to
+ *                                "epsilon hat" in the Kingma and Ba paper (in the formula just before Section 2.1),
+ *                                and not to the epsilon in Algorithm 1 of the paper.
+ * @param  useLocking             If `true`, the gradient descent updates will be protected by a lock. Otherwise, the
+ *                                behavior is undefined, but may exhibit less contention.
+ * @param  learningRateSummaryTag Optional summary tag name to use for the learning rate value. If `null`, no summary
+ *                                is created for the learning rate. Otherwise, a scalar summary is created which can
+ *                                be monitored using TensorBoard.
+ * @param  name                   Name for this optimizer.
+ *
+ * @author Emmanouil Antonios Platanios
+ */
+open class AdamOptimizer(
+    val learningRate: () -> Double = { 0.001 },
+    val beta1: Double = 0.9,
+    val beta2: Double = 0.999,
+    val epsilon: Double = 1e-8,
+    override val useLocking: Boolean = false,
+    val learningRateSummaryTag: String? = null,
+    override val name: String = "Adam"
+) : Optimizer() {
+  
+  override val ignoreDuplicateSparseIndices = true
+  lateinit var learningRateTensor: Output
+  lateinit var beta1Tensor: Output
+  lateinit var beta2Tensor: Output
+  lateinit var epsilonTensor: Output
+  
+  protected fun getBetaPowerAccumulators(): Pair<Variable, Variable> =
+      getNonSlotVariable("beta1_power", tf.currentGraph)!! to
+          getNonSlotVariable("beta2_power", tf.currentGraph)!!
+  
+  override fun createSlots(variables: List<Variable>) {
+    val firstVar = variables.minBy { it.name }!!
+    getOrCreateNonSlotVariable("beta1_power", Tensor(beta1), mutableSetOf(firstVar.op))
+    getOrCreateNonSlotVariable("beta2_power", Tensor(beta2), mutableSetOf(firstVar.op))
+    variables.forEach { v ->
+      zerosSlot("m", v, name)
+      zerosSlot("v", v, name)
+    }
+  }
+  
+  override fun prepare(iteration: Variable?) {
+    learningRateTensor = tf.const(learningRate(), "learning_rate")
+    beta1Tensor = tf.const(beta1, "beta1")
+    beta2Tensor = tf.const(beta2, "beta2")
+    epsilonTensor = tf.const(epsilon, "epsilon")
+  }
+  
+  override fun applyDense(gradient: Output, variable: Variable, iteration: Variable?): Op {
+    val m = getSlot("m", variable)!!
+    val v = getSlot("v", variable)!!
+    val (beta1Power, beta2Power) = getBetaPowerAccumulators()
+    return tf.applyAdam(variable.variable, m.variable, v.variable,
+                        tf.cast(beta1Power.value, variable.dataType.baseDataType),
+                        tf.cast(beta2Power.value, variable.dataType.baseDataType),
+                        tf.cast(learningRateTensor, variable.dataType.baseDataType),
+                        tf.cast(beta1Tensor, variable.dataType.baseDataType),
+                        tf.cast(beta2Tensor, variable.dataType.baseDataType),
+                        tf.cast(epsilonTensor, variable.dataType.baseDataType),
+                        gradient,
+                        useLocking).op
+  }
+  
+  override fun applySparse(gradient: IndexedSlices, variable: Variable, iteration: Variable?): Op {
+    val (beta1Power, beta2Power) = getBetaPowerAccumulators()
+    val beta1_power = tf.cast(beta1Power.value, variable.dataType.baseDataType)
+    val beta2_power = tf.cast(beta2Power.value, variable.dataType.baseDataType)
+    val lr_t = tf.cast(learningRateTensor, variable.dataType.baseDataType)
+    val beta1_t = tf.cast(beta1Tensor, variable.dataType.baseDataType)
+    val beta2_t = tf.cast(beta2Tensor, variable.dataType.baseDataType)
+    val epsilon_t = tf.cast(epsilonTensor, variable.dataType.baseDataType)
+    val lr = (lr_t * tf.sqrt(1 - beta2_power) / (1 - beta1_power))
+    // m_t = beta1 * m + (1 - beta1) * g_t
+    val m = getSlot("m", variable)!!
+    val m_scaled_g_values = gradient.values * (1 - beta1_t)
+    var m_t = tf.assign(m.variable, m * beta1_t,
+                        useLocking = useLocking)
+    tf.controlDependencies(m_t) {
+      m_t = tf.scatterAdd(m.variable, gradient.indices, m_scaled_g_values)
+    }
+    // v_t = beta2 * v + (1 - beta2) * (g_t * g_t)
+    val v = getSlot("v", variable)!!
+    val v_scaled_g_values = (gradient.values * gradient.values) * (1 - beta2_t)
+    var v_t = tf.assign(v.variable, v * beta2_t, useLocking = useLocking)
+    tf.controlDependencies(v_t) {
+      v_t = tf.scatterAdd(v.variable, gradient.indices, v_scaled_g_values)
+    }
+    val v_sqrt = tf.sqrt(v_t)
+    val var_update = tf.assignSub(variable.variable, lr * m_t / (v_sqrt + epsilon_t),
+                                  useLocking = useLocking)
+    return tf.group(setOf(var_update.op, m_t.op, v_t.op))
+  }
+  
+  override fun finish(updateOps: Set<Op>, nameScope: String): Op {
+    val updateBetaOps = tf.controlDependencies(updateOps.toMutableSet()) {
+      val (beta1Power, beta2Power) = getBetaPowerAccumulators()
+      tf.colocateWith(beta1Power.op) {
+        val updateBeta1 = beta1Power.assign(beta1Power * beta1Tensor)
+        val updateBeta2 = beta2Power.assign(beta2Power * beta2Tensor)
+        setOf(updateBeta1.op, updateBeta2.op)
+      }
+    }
+    return tf.group(updateOps + updateBetaOps, nameScope)
+  }
+}
 //
 //import wumo.sim.tensorflow.*
 //import wumo.sim.tensorflow.ops.*
--- tensorflow/src/test/kotlin/wumo/sim/tensorflow/ops/GradientsOldKtTest.kt
+++ tensorflow/src/test/kotlin/wumo/sim/tensorflow/ops/GradientsOldKtTest.kt
@@ -1,6 +1,7 @@
 package wumo.sim.tensorflow.ops
 
 import org.junit.Test
+import wumo.sim.tensorflow.ops.training.AdamOptimizer
 import wumo.sim.tensorflow.ops.training.GradientDescentOptimizer
 import wumo.sim.tensorflow.tf
 
@@ -16,7 +17,7 @@ class GradientsOldKtTest : BaseTest() {
     printGraph()
     tf.session {
       init.run()
-      repeat(10) {
+      repeat(20) {
         update.run()
         val (_x, _y) = eval<Float, Float>(x.toOutput(), y)
         println("$_y,$_x")
@@ -26,25 +27,25 @@ class GradientsOldKtTest : BaseTest() {
   
   @Test
   fun gradientDescentOptimizer() {
-//    val x = tf.variable(1f, name = "x")
-//    val y = tf.variable(1f, name = "y")
-//    val z = tf.mul(x, y, "z")
-//    val optimizer = GradientDescentOptimizer(learningRate = 0.1f)
-//    val opt = optimizer.minimize(z)
-//    val init = tf.globalVariablesInitializer()
-//    println(tf.debugString())
-//    tf.session {
-//      init.run()
-//      x.eval()
-//      y.eval()
-//      z.eval()
-//      for (i in 0 until 10) {
-//        println(i)
-//        opt.run()
-//        x.eval()
-//        y.eval()
-//        z.eval()
-//      }
-//    }
+    val x = tf.variable(1f, name = "x")
+    val y = tf.variable(1f, name = "y")
+    val z = tf.mul(x.toOutput(), y.toOutput(), "z")
+    val optimizer = AdamOptimizer(learningRate = { 0.1 })
+    val opt = optimizer.minimize(z)
+    val init = tf.globalVariablesInitializer()
+    println(tf.debugString())
+    tf.session {
+      init.run()
+      x.eval()
+      y.eval()
+      z.eval()
+      for (i in 0 until 10) {
+        println(i)
+        opt.run()
+        x.eval()
+        y.eval()
+        z.eval()
+      }
+    }
   }
 }
